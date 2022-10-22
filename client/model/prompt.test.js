babb9117fdeffba21dff5e541a94336efb7bc8e3
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-10-22 10:33:49 -0400

    Rename QuestionQueue to PromptQueue

--- client/model/prompt.test.js
+++ client/model/prompt.test.js
@@ -2,22 +2,22 @@ import { PromptQueue } from './prompt'
 
 describe('create question story', () => {
   it('when I create a question, then I should receive a prompt at the times I set', () => {
-    const questionQueue = PromptQueue()
+    const promptQueue = PromptQueue()
 
-    questionQueue.createQuestion({ id: 1, question: 'Did you study 2 hours today?' })
-    let promptList = questionQueue.query() //Changed from const to let.
+    promptQueue.createQuestion({ id: 1, question: 'Did you study 2 hours today?' })
+    let promptList = promptQueue.query() //Changed from const to let.
 
     expect(promptList).toEqual([{ questionId: 1, question: 'Did you study 2 hours today?' }])
 
     // given a prompt, when I enter an answer
     const answer = { questionId: 1, id: 1, answer: true }
-    questionQueue.answerPrompt(answer)
+    promptQueue.answerPrompt(answer)
 
     // then the answer is recorded
-    expect(questionQueue.getAnswers()).toEqual([answer])
+    expect(promptQueue.getAnswers()).toEqual([answer])
 
     // if a prompt has been answered, then it should be filtered out of the prompt list
-    promptList = questionQueue.query()
+    promptList = promptQueue.query()
 
     expect(promptList).toEqual([])
   })
