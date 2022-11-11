a4909e4494bb28baa0b674fcde257dcc9242d994
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-11-10 22:01:19 -0500

    Fix argument order

--- client/personal-data-collection/domain/index.ts
+++ client/personal-data-collection/domain/index.ts
@@ -1,5 +1,5 @@
+import { PromptQueue } from './usecases/prompt-queue'
 import answerRepositoryInMemory from '../infrastructure/answer-repository-in-memory'
 import recurringQuestionRepositoryInMemory from '../infrastructure/recurring-question-repository-in-memory'
-import { PromptQueue } from './usecases/prompt-queue'
 
-export const promptQueue = PromptQueue(answerRepositoryInMemory())(recurringQuestionRepositoryInMemory())
+export const promptQueue = PromptQueue(recurringQuestionRepositoryInMemory())(answerRepositoryInMemory())
