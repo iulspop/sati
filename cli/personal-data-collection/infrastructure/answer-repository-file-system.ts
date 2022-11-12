7bb7de1582f8b6294072adec6ad22f6946c7cf8f
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-11-12 15:55:52 -0500

    Remove logs

--- cli/personal-data-collection/infrastructure/answer-repository-file-system.ts
+++ cli/personal-data-collection/infrastructure/answer-repository-file-system.ts
@@ -14,21 +14,13 @@ export default function answerRepositoryFileSystem(): AnswerRepository {
     fs.writeFileSync(path.join(storageDirPath, 'answers.json'), JSON.stringify([]))
 
   return {
-    findMany: async () => {
-      const string = fs.readFileSync(path.join(storageDirPath, 'answers.json'), 'utf8')
-      console.log('answers string:', string)
-      const answers = JSON.parse(string)
-      console.log('findMany:', answers)
-      return answers.map(answer => ({
+    findMany: async () =>
+      JSON.parse(fs.readFileSync(path.join(storageDirPath, 'answers.json'), 'utf8')).map(answer => ({
         ...answer,
         timestamp: new Date(answer.timestamp),
-      }))
-    },
+      })),
     create: async answer => {
       const answers = JSON.parse(fs.readFileSync(path.join(storageDirPath, 'answers.json'), 'utf8'))
-      console.log('----')
-      console.log('findMany:', answers)
-      console.log('create:', answer)
       fs.writeFileSync(path.join(storageDirPath, 'answers.json'), JSON.stringify([...answers, createAnswer(answer)]))
     },
   }
