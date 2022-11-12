e8a88ea971908802c39d07c81c4f9e5bc6511185
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-11-12 16:08:11 -0500

    Extract functions from CLI query

--- cli/personal-data-collection/controllers/cli-to-prompt-queue.ts
+++ cli/personal-data-collection/controllers/cli-to-prompt-queue.ts
@@ -6,31 +6,29 @@ const cliToPromptQueue = {
   createRecurringQuestion: async argv => await promptQueue.createRecurringQuestion({ question: argv.question }),
   query: async () => {
     const promptList = await promptQueue.query(addDay(new Date()))
-
-    const mapPromptsToQuestions = promptList =>
-      promptList.map(({ question }, index) => ({
-        type: 'toggle',
-        name: String(index),
-        message: question,
-        initial: true,
-        active: 'yes',
-        inactive: 'no',
-      }))
-
     const response: { '0': boolean } = await prompts(mapPromptsToQuestions(promptList))
-
-    const mapResponseToAnswers = (promptList, response: { '0': boolean }) =>
-      Object.entries(response).map(([index, response]) => {
-        const { questionId, timestamp } = promptList[index]
-        return {
-          questionId,
-          response,
-          timestamp,
-        }
-      })
-
     mapResponseToAnswers(promptList, response).forEach(answer => promptQueue.answerPrompt(answer))
   },
 }
+
+const mapPromptsToQuestions = promptList =>
+  promptList.map(({ question }, index) => ({
+    type: 'toggle',
+    name: String(index),
+    message: question,
+    initial: true,
+    active: 'yes',
+    inactive: 'no',
+  }))
+
+const mapResponseToAnswers = (promptList, response: { '0': boolean }) =>
+  Object.entries(response).map(([index, response]) => {
+    const { questionId, timestamp } = promptList[index]
+    return {
+      questionId,
+      response,
+      timestamp,
+    }
+  })
 
 export default cliToPromptQueue
