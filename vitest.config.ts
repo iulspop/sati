162a58bedef726d33dea62184252c737782d0c97
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-11-19 18:53:17 -0500

    Exclude bin from tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     watchExclude: ['./storage/**'],
+    exclude: ['node_modules', 'bin'],
   },
   resolve: {
     alias: {
