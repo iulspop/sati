fd6bccb59e9d968587543d6096a6533483bbf05d
Radu <raduadrian.constantin94@gmail.com>
2022-12-07 21:09:46 +0200

    Implemented recurring question prisma api. Did not have a chance to test yet

--- cli/personal-data-collection/domain/repositories/recurring-question-repository.d.ts
+++ cli/personal-data-collection/domain/repositories/recurring-question-repository.d.ts
@@ -1,6 +1,6 @@
 import RecurringQuestion from '../entities/recurring-question'
 
 export default interface RecurringQuestionRepository {
-  findMany(): Promise<Array<RecurringQuestion>>
-  create(recurringQuestion: Partial<RecurringQuestion>): Promise<void>
+  findMany(): Array<RecurringQuestion>
+  create(recurringQuestion: Partial<RecurringQuestion>)
 }
