f0169efe0c2b7971fc9c840aac3a1f51d3fcc591
Radu <raduadrian.constantin94@gmail.com>
2022-12-08 20:10:51 +0200

    Fixed TypeScript errors in recurring-question-prisma-api and separated prisma initialization to separate module

--- cli/personal-data-collection/domain/index.ts
+++ cli/personal-data-collection/domain/index.ts
@@ -1,5 +1,6 @@
 import { PromptQueue } from './usecases/prompt-queue.js'
 import recurringQuestionRepositoryFileSystem from '../infrastructure/recurring-question-repository-file-system.js'
 import answerRepositoryFileSystem from '../infrastructure/answer-repository-file-system.js'
+import recurringQuestionRepositoryDatabase from '../infrastructure/recurring-question-prisma-api.js'
 
-export const promptQueue = PromptQueue(recurringQuestionRepositoryFileSystem())(answerRepositoryFileSystem())
+export const promptQueue = PromptQueue(recurringQuestionRepositoryDatabase())(answerRepositoryFileSystem())
--- cli/personal-data-collection/infrastructure/recurring-question-prisma-api.ts
+++ cli/personal-data-collection/infrastructure/recurring-question-prisma-api.ts
@@ -1,82 +1,47 @@
-import AnswerRepository from '../domain/repositories/answer-repository.js'
-import { createAnswer } from '../domain/entities/answer.js'
+import prisma from './prisma-client';
 
-import { PrismaClient } from '@prisma/client'
 import RecurringQuestionRepository from '../domain/repositories/recurring-question-repository.js'
-import { UrlWithStringQuery } from 'url'
-
-const prisma = new PrismaClient()
-async function main() {
-  // const newQuestions = await prisma.recurringQuestion.create({
-  //   data: {
-  //     question: 'Have you studied today?',
-  //     utcOffsetInMinutes: 120
-  //   },
-  // })
-
-  // console.log(newQuestion)
-  //
-
-   const newAnswer = await prisma.answer.create({
-    data: {
-      questionId: '957eebd3-2560-462a-aec4-42ab00666a71',
-      response: true
-    },
-  })
-
-  console.log(newAnswer)
-  
-  // const questions = await prisma.recurringQuestion.findMany();
-  // console.log(questions);
-}
 
 export default function recurringQuestionRepositoryDatabase(): RecurringQuestionRepository {
   return {
-    findMany: async() => {
-      const recurringQuestions = await prisma.recurringQuestion.findMany();
-      return recurringQuestions.map(recurringQuestion => {
-        return {
-          id: recurringQuestion.id,
-          question: recurringQuestion.question,
-          phases: {
-            timestamp: new Date(recurringQuestion.timestamp),
-            utcOffsetInMinutes: recurringQuestion.utcOffsetInMinutes
+    findMany: async () => {
+      try {
+        const recurringQuestions = await prisma.recurringQuestion.findMany();
+        const mappedRecurringQuestions = recurringQuestions.map(recurringQuestion => {
+          return {
+            id: recurringQuestion.id,
+            question: recurringQuestion.question,
+            phases: [{
+              timestamp: new Date(recurringQuestion.timestamp),
+              utcOffsetInMinutes: recurringQuestion.utcOffsetInMinutes
+            }]
           }
-        }
-      })
+        })
+        await prisma.$disconnect();
+        return new Promise((resolve, reject) => {
+          resolve(mappedRecurringQuestions);
+        });
+      } catch (e) {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+      }
     },
-    create: async(recurringQuestion) => {
-      await prisma.recurringQuestion.create({data: {
-        ...recurringQuestion
-      }})
+    create: async (recurringQuestion) => {
+      try {
+        await prisma.recurringQuestion.create({
+          data: {
+            question: recurringQuestion.question,
+            timestamp: recurringQuestion.phases[0].timestamp,
+            utcOffsetInMinutes: recurringQuestion.phases[0].utcOffsetInMinutes
+          }
+        })
+        await prisma.$disconnect();
+      } catch (e) {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+      }
     }
   }
 }
-
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
-
-// export default function answerRepositoryFileSystem(): AnswerRepository {
-//   if (!fs.existsSync(storageDirPath)) fs.mkdirSync(storageDirPath)
-//   if (!fs.existsSync(path.join(storageDirPath, 'answers.json')))
-//     fs.writeFileSync(path.join(storageDirPath, 'answers.json'), JSON.stringify([]))
-
-//   return {
-//     findMany: async () =>
-//       JSON.parse(fs.readFileSync(path.join(storageDirPath, 'answers.json'), 'utf8')).map(answer => ({
-//         ...answer,
-//         timestamp: new Date(answer.timestamp),
-//       })),
-//     create: async answer => {
-//       const answers = JSON.parse(fs.readFileSync(path.join(storageDirPath, 'answers.json'), 'utf8'))
-//       fs.writeFileSync(path.join(storageDirPath, 'answers.json'), JSON.stringify([...answers, createAnswer(answer)], null, 2))
-//     },
-//   }
-// }
