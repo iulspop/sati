44444b311c6e0ca24376b66cfd07478ec7b8ef90
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-12-10 09:49:10 -0500

    Extract prisma client module
    Co-authored-by: Radu Constantin <radu-constantin@users.noreply.github.com>

--- cli/personal-data-collection/infrastructure/recurring-question-prisma-api.ts
+++ cli/personal-data-collection/infrastructure/recurring-question-prisma-api.ts
@@ -1,48 +1,48 @@
 import RecurringQuestionRepository from '../domain/repositories/recurring-question-repository.js'
-
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import prisma from './prisma-client.js'
 
 export default function recurringQuestionRepositoryDatabase(): RecurringQuestionRepository {
   return {
     findMany: async () => {
       try {
-        const recurringQuestions = await prisma.recurringQuestion.findMany();
+        const recurringQuestions = await prisma.recurringQuestion.findMany()
         const mappedRecurringQuestions = recurringQuestions.map(recurringQuestion => {
           return {
             id: recurringQuestion.id,
             question: recurringQuestion.question,
-            phases: [{
-              timestamp: new Date(recurringQuestion.timestamp),
-              utcOffsetInMinutes: recurringQuestion.utcOffsetInMinutes
-            }]
+            phases: [
+              {
+                timestamp: new Date(recurringQuestion.timestamp),
+                utcOffsetInMinutes: recurringQuestion.utcOffsetInMinutes,
+              },
+            ],
           }
         })
-        await prisma.$disconnect();
+        await prisma.$disconnect()
         return new Promise((resolve, reject) => {
-          resolve(mappedRecurringQuestions);
-        });
+          resolve(mappedRecurringQuestions)
+        })
       } catch (e) {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
       }
     },
-    create: async (recurringQuestion) => {
+    create: async recurringQuestion => {
       try {
         await prisma.recurringQuestion.create({
           data: {
             question: recurringQuestion.question,
             timestamp: recurringQuestion.phases[0].timestamp,
-            utcOffsetInMinutes: recurringQuestion.phases[0].utcOffsetInMinutes
-          }
+            utcOffsetInMinutes: recurringQuestion.phases[0].utcOffsetInMinutes,
+          },
         })
-        await prisma.$disconnect();
+        await prisma.$disconnect()
       } catch (e) {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
       }
-    }
+    },
   }
 }
