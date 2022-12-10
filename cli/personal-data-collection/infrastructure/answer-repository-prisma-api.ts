e487df3fcf0fc25ca93df135a4e6ece6dae2f956
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-12-10 10:07:39 -0500

    Refactor Prisma repositories
    Co-authored-by: Radu Constantin <radu-constantin@users.noreply.github.com>

--- cli/personal-data-collection/infrastructure/answer-repository-prisma-api.ts
+++ cli/personal-data-collection/infrastructure/answer-repository-prisma-api.ts
@@ -3,46 +3,15 @@ import prisma from './prisma-client.js'
 
 export default function answerRepositoryDatabase(): AnswerRepository {
   return {
-    findMany: async () => {
-      try {
-        const answers = await prisma.answer.findMany()
-
-        // const mappedAnswers = answers.map(answer => {
-        //   return {
-        //     id: answer.id,
-        //     question: answer.question,
-        //     phases: [{
-        //       timestamp: new Date(answer.timestamp),
-        //       utcOffsetInMinutes: answer.utcOffsetInMinutes
-        //     }]
-        //   }
-        // })
-
-        await prisma.$disconnect()
-        return new Promise((resolve, reject) => {
-          resolve(answers)
-        })
-      } catch (e) {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-      }
-    },
+    findMany: async () => await prisma.answer.findMany(),
     create: async answer => {
-      try {
-        await prisma.answer.create({
-          data: {
-            questionId: answer.questionId,
-            response: answer.response,
-            timestamp: new Date(answer.timestamp),
-          },
-        })
-        await prisma.$disconnect()
-      } catch (e) {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-      }
+      await prisma.answer.create({
+        data: {
+          questionId: answer.questionId,
+          response: answer.response,
+          timestamp: new Date(answer.timestamp),
+        },
+      })
     },
   }
 }
