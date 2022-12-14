ed727ad1dceb7e035e55427f4de213b4b429acc8
Radu <raduadrian.constantin94@gmail.com>
2022-12-14 19:13:08 +0200

    Deleted file system imports from index.ts

--- cli/personal-data-collection/domain/index.ts
+++ cli/personal-data-collection/domain/index.ts
@@ -1,6 +1,4 @@
 import { PromptQueue } from './usecases/prompt-queue.js'
-import recurringQuestionRepositoryFileSystem from '../infrastructure/recurring-question-repository-file-system.js'
-import answerRepositoryFileSystem from '../infrastructure/answer-repository-file-system.js'
 import recurringQuestionRepositoryDatabase from '../infrastructure/recurring-question-prisma-api.js'
 import answerRepositoryDatabase from '../infrastructure/answer-repository-prisma-api.js'
 
