36fbcab13296a9cd65df3deea3239057570287fa
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-12-23 17:02:20 -0500

    Move personal-data-collection out of domains folder

--- src/web/remix-app/app/routes/index.tsx
+++ src/web/remix-app/app/routes/index.tsx
@@ -1,33 +1,37 @@
-import { useLoaderData } from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react'
 // import promptQueueInstance from 'utils/db.server';
-import prisma from '../../../../domains/personal-data-collection/infrastructure/prisma-client';
+import prisma from '../../../../personal-data-collection/infrastructure/prisma-client'
 
 export const loader = async () => {
   const data = await prisma.recurringQuestion.findMany({
     include: {
       answers: true,
-    }
-  });
+    },
+  })
 
-  return data;
+  return data
 }
 
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
+  const data = useLoaderData<typeof loader>()
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       {data.map(question => {
         let answers = question.answers.map(answer => {
-          return <p>{answer.timestamp}: {String(answer.response)}</p>
-        });
+          return (
+            <p>
+              {answer.timestamp}: {String(answer.response)}
+            </p>
+          )
+        })
         return (
           <div>
             <h2>{question.question}</h2>
             <h3>{answers}</h3>
           </div>
-        );
+        )
       })}
     </div>
-  );
+  )
 }
