83d80c720cd8fff1b5a6580202bd7b0ee67c49a5
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-12-23 17:00:09 -0500

    Rename usecases to services

--- src/domains/personal-data-collection/domain/index.ts
+++ src/domains/personal-data-collection/domain/index.ts
@@ -1,4 +1,4 @@
-import { PromptQueue } from './usecases/prompt-queue.js'
+import { PromptQueue } from './services/prompt-queue.js'
 import recurringQuestionRepositoryDatabase from '../infrastructure/recurring-question-prisma-api.js'
 import answerRepositoryDatabase from '../infrastructure/answer-repository-prisma-api.js'
 
