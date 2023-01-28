b9868097a8258406fef03e7d26945f5b31201a7b
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-01-28 12:33:50 -0500

    Quick commit

--- domains/self-observability/infrastructure/slo-prisma.ts
+++ domains/self-observability/infrastructure/slo-prisma.ts
@@ -2,8 +2,8 @@ import SLORepositoryAPI from '../domain/repositories/slo-repository'
 import db from '../../db.server'
 
 export const SLORepository = (): SLORepositoryAPI => ({
-  create: async slo => await db.slo.create({ data: { ...slo } }),
+  create: async slo => await db.slo.create({ data: slo }),
   read: async id => (id ? await db.slo.findUnique({ where: { id } }) : await db.slo.findMany()),
-  update: async (id, slo) => await db.slo.update({ where: { id }, data: { ...slo } }),
+  update: async (id, slo) => await db.slo.update({ where: { id }, data: slo }),
   delete: async id => await db.slo.delete({ where: { id } }),
 })
