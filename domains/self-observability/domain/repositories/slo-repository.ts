be211093d0e4f51b660810d36d458d3c25143829
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-01-28 11:47:17 -0500

    Fix SLORepo.read type

--- domains/self-observability/domain/repositories/slo-repository.ts
+++ domains/self-observability/domain/repositories/slo-repository.ts
@@ -2,7 +2,7 @@ import { SLO } from '../entities/slo'
 
 export default interface SLORepositoryAPI {
   create(slo: SLO): Promise<SLO>
-  read(id?: string): Promise<SLO> | Promise<SLO[]> | null
+  read(id?: string): Promise<SLO | SLO[] | null>
   update(id: string, slo: Partial<SLO>): Promise<SLO>
   delete(id: string): Promise<SLO>
 }
