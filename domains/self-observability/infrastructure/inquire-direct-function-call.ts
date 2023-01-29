e99248e43941862416850d64905dd60c52ffc2ab
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-01-29 18:08:48 -0500

    Add inquire direct method  call repo implementation

---
+++ domains/self-observability/infrastructure/inquire-direct-function-call.ts
@@ -0,0 +1,6 @@
+import { promptQueue } from '@domains/self-data-collection/domain'
+import { InquireRepositoryAPI } from '../domain/repositories/inquire-repository'
+
+export const eventRepository: InquireRepositoryAPI = {
+  getAnswers: promptQueue.getAnswers,
+}
