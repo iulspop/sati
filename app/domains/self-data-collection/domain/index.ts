36a445c8ef7bd5fa69115191dcd4c8e0f95316ec
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-02-11 11:14:44 -0500

    Update self-data-collection index

--- app/domains/self-data-collection/domain/index.ts
+++ app/domains/self-data-collection/domain/index.ts
@@ -1,5 +1,9 @@
 import { AnswerRepository } from '../infrastructure/answer-prisma.js'
 import { RecurringQuestionRepository } from '../infrastructure/recurring-question-prisma.js'
+import { Answers } from './services/answers.js'
 import { PromptQueue } from './services/prompt-queue.js'
+import { RecurringQuestions } from './services/recurring-questions.js'
 
-export const promptQueue = PromptQueue(RecurringQuestionRepository())(AnswerRepository())
+export const recurringQuestions = RecurringQuestions(RecurringQuestionRepository())
+export const answers = Answers(AnswerRepository())
+export const promptQueue = PromptQueue(recurringQuestions)(answers)
