cdc9b694f777b433573af6e456d53d6b701dca86
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-03-04 10:53:19 -0500

    build: add questions to seed script

--- app/domains/self-data-collection/domain/index.ts
+++ app/domains/self-data-collection/domain/index.ts
@@ -1,8 +1,8 @@
-import { AnswerRepository } from '../infrastructure/answer-prisma.js'
-import { RecurringQuestionRepository } from '../infrastructure/recurring-question-prisma.js'
-import { Answers as Answers_ } from './services/answers.js'
-import { PromptQueue as PromptQueue_ } from './services/prompt-queue.js'
-import { RecurringQuestions as RecurringQuestions_ } from './services/recurring-questions.js'
+import { AnswerRepository } from '../infrastructure/answer-prisma'
+import { RecurringQuestionRepository } from '../infrastructure/recurring-question-prisma'
+import { Answers as Answers_ } from './services/answers'
+import { PromptQueue as PromptQueue_ } from './services/prompt-queue'
+import { RecurringQuestions as RecurringQuestions_ } from './services/recurring-questions'
 
 export const RecurringQuestions = RecurringQuestions_(RecurringQuestionRepository())
 export const Answers = Answers_(AnswerRepository())
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,12 +3,21 @@ import 'dotenv/config'
 import { faker } from '@faker-js/faker'
 import { PrismaClient } from '@prisma/client'
 import { exit } from 'process'
+import { RecurringQuestions } from '~/domains/self-data-collection/domain'
 
 const prettyPrint = (object: any) => console.log(JSON.stringify(object, undefined, 2))
 
 const prisma = new PrismaClient()
 
 const userId = process.env.SEED_USER_ID
+
+const daysAgo = (days: number) => {
+  const date = new Date()
+  date.setDate(date.getDate() - days)
+  return date
+}
+
+const utcOffsetInMinutes = new Date().getTimezoneOffset()
 
 async function seed() {
   if (!userId) {
@@ -22,8 +31,22 @@ async function seed() {
       name: faker.name.fullName(),
     },
   })
+
+  await RecurringQuestions.create({
+    question: 'Did you go to bed at 9:30PM?',
+    phase: { timestamp: daysAgo(3), utcOffsetInMinutes },
+  })
+
+  await RecurringQuestions.create({
+    question: 'Did you eat last meal by 3PM?',
+    phase: { timestamp: daysAgo(2), utcOffsetInMinutes },
+  })
+
+  const questions = await RecurringQuestions.readAll()
+
   console.log('========= result of seed: =========')
   prettyPrint({ user })
+  prettyPrint(questions)
 }
 
 seed()
