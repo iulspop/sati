fbfe97f57be982a64d0b9ad63b0276d697369b0e
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-05-19 12:39:24 -0400

    refactor: inline home page component in nested layout route

--- app/routes/_home/route.test.tsx
+++ app/routes/_home/route.test.tsx
@@ -1,7 +1,7 @@
 import { unstable_createRemixStub as createRemixStub } from '@remix-run/testing'
 import { render, screen, within } from '@testing-library/react'
 import { describe, expect, test } from 'vitest'
-import HomePage from './route'
+import HomePage, { calculateIfLinkIsCurrent } from './route'
 
 describe('Home page', () => {
   test("given initial load: render the navigation bar with links to 'Recurring Questions' and 'Prompt Queue'", async () => {
@@ -32,5 +32,24 @@ describe('Home page', () => {
     const navigation = screen.getByRole('navigation')
     expect(within(navigation).getByText('Recurring Questions')).not.toHaveAttribute('aria-current')
     expect(within(navigation).getByText('Prompt Queue')).toHaveAttribute('aria-current', 'page')
+  })
+})
+
+describe('calculateIfLinkIsCurrent()', () => {
+  test('given an equal path and href: returns true', () => {
+    expect(calculateIfLinkIsCurrent({ path: '/queue', href: '/queue' })).toEqual(true)
+  })
+
+  test('given an unequal path and href: returns false', () => {
+    expect(calculateIfLinkIsCurrent({ path: '/queue', href: '/' })).toEqual(false)
+  })
+
+  test('given an href is a subpath of the path: returns true', () => {
+    expect(calculateIfLinkIsCurrent({ path: '/questions', href: '/questions/123' })).toEqual(true)
+  })
+
+  test('given an href that is not a subpath of the path: returns false', () => {
+    expect(calculateIfLinkIsCurrent({ path: '/questions', href: '/question' })).toEqual(false)
+    expect(calculateIfLinkIsCurrent({ path: '/questions', href: '/questions123' })).toEqual(false)
   })
 })
