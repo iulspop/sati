05ae54f57897e55fc492ee0d45e78145d9230ce7
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-09-26 13:31:17 -0400

    devx: add devx tag to commitlint settings

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,11 @@ const config = {
   rules: {
     'footer-max-line-length': [0, 'always'],
     'body-max-line-length': [0, 'always'],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'devx'],
+    ],
   },
 }
 
