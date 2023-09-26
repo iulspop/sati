44672ef99000baaf5d424c6cd36d181c68db712e
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-09-26 11:47:21 -0400

    feat: style edit question form

--- app/routes/_home.questions_.$questionId/edit-question-form-component.tsx
+++ app/routes/_home.questions_.$questionId/edit-question-form-component.tsx
@@ -11,14 +11,20 @@ export function EditQuestionFormComponent({ id, text }: EditQuestionFormComponen
   const fetcher = useFetcher()
 
   return (
-    <div>
-      <h2>Edit Question:</h2>
-      <p>{text}</p>
-      <div>
-        <Link to="/questions">Cancel</Link>
-        <fetcher.Form method="delete">
+    <div className="flex flex-col space-y-4 bg-white p-6 rounded shadow-lg w-full max-w-md mx-auto dark:text-black">
+      <h2 className="font-semibold text-lg">Edit Question:</h2>
+      <p className="border p-2 rounded dark:bg-gray-200 dark:border-gray-500 dark:text-black">{text}</p>
+
+      <div className="flex justify-between items-center">
+        <Link to="/questions" className="text-blue-600 hover:text-blue-700 underline">
+          Cancel
+        </Link>
+
+        <fetcher.Form method="delete" className="flex">
           <input type="hidden" name="questionId" value={id} />
-          <button type="submit">Delete</button>
+          <button type="submit" className="bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-700">
+            Delete
+          </button>
         </fetcher.Form>
       </div>
     </div>
