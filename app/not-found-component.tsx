5a13c189728ff4c0cd4fd402784f4cabfe8db4aa
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-09-26 13:40:39 -0400

    fix: color contrast not found page and index page dark mode

--- app/not-found-component.tsx
+++ app/not-found-component.tsx
@@ -17,7 +17,7 @@ export function NotFoundComponent() {
 
         <div className="py-16">
           <div className="text-center">
-            <p className="text-sm font-semibold uppercase tracking-wide text-indigo-600 dark:text-indigo-500">
+            <p className="text-sm font-semibold uppercase tracking-wide text-indigo-600 dark:text-indigo-200">
               404 Error
             </p>
             <h1 className="mt-2 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-5xl">
@@ -31,7 +31,7 @@ export function NotFoundComponent() {
             <div className="mt-6">
               <Link
                 to={'/'}
-                className="text-base font-medium text-indigo-600 hover:text-indigo-500 dark:text-indigo-500 dark:hover:text-indigo-400"
+                className="text-base font-medium text-indigo-600 hover:text-indigo-400 dark:text-indigo-200 dark:hover:text-indigo-300"
               >
                 Go back home
                 <span aria-hidden="true">&rarr;</span>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,11 +18,10 @@ export const loader = async ({ request }: LoaderArgs) => {
 export default function LandingPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center space-y-12 bg-white p-10 dark:bg-slate-800">
-      <h1>Inquire</h1>
-      <p>Hello!</p>
+      <h1 className="dark:text-white">Sati</h1>
       <Link
         to="/login"
-        className="flex max-w-lg items-center justify-center rounded-md bg-indigo-600 px-4 py-3 font-medium text-white "
+        className="flex max-w-lg items-center justify-center rounded-md bg-indigo-600 px-4 py-3 font-medium text-white"
       >
         Log In
       </Link>
