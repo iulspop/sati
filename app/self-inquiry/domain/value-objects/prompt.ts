e162281005a763718e1fa04cc72dab5226e13c1f
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-09-30 17:36:22 -0400

    fix: zod transform timestamp to Date

--- app/self-inquiry/domain/value-objects/prompt.ts
+++ app/self-inquiry/domain/value-objects/prompt.ts
@@ -1,11 +1,9 @@
 import { z } from 'zod'
 
-export const PromptSchema = z
-  .object({
-    questionId: z.string(),
-    text: z.string(),
-    timestamp: z.date(),
-  })
-  .required()
+export const PromptSchema = z.object({
+  questionId: z.string(),
+  text: z.string(),
+  timestamp: z.string().transform(str => new Date(str)),
+})
 
 export type Prompt = z.infer<typeof PromptSchema>
